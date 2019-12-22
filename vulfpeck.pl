6e5a9b20c21364ee12d30b97a8ab8cb092dfb4c1
Carlos Vagner <47748982+glitchysnitchy@users.noreply.github.com>
2019-12-22 14:40:41 -0300

    Base de conhecimento

---
+++ vulfpeck.pl
@@ -0,0 +1,24 @@
+bass(joedart).
+cowbell(woodygoss).
+drums(theokatzman).
+drums(jackstratton).
+drums(louiscole).
+guitar(theokatzman).
+guitar(jackstratton).
+guitar(corywong).
+piano(jackstratton).
+piano(woodygoss).
+piano(joeydosik).
+vocals(antwaunstanley).
+vocals(theokatzman).
+vocals(joeydosik).
+wurlitzer(jackstratton).
+wurlitzer(woodygoss).
+
+beastly(W,B,D,C) :- wurlitzer(W), bass(B), drums(D), cowbell(C).
+itgetsfunkier(P,B,D,W):- piano(P), bass(B), drums(D), wurlitzer(W).
+sixteentwelve(V,G,B,D,W) :- vocals(V), guitar(G), bass(B), drums(D), wurlitzer(W).
+
+
+
+
